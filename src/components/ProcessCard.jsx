9ec3f6a07746d482ea29bd67ba7fdabca7fab4f0
Musa Zeshan <musazeshan.it@gmail.com>
2024-06-20 10:34:41 +0500

    Now added the shadow to all sides of the process cards.

--- src/components/ProcessCard.jsx
+++ src/components/ProcessCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ProcessCard = ({ imgUrl, title, desc, stepNum }) => {
     return (
-        <div className='h-[220px] w-[250px] shadow-xl rounded-sm bg-[rgb(254,254,254)]'>
+        <div className='h-[220px] w-[250px] shadow-[0px_0px_16px_rgba(0,0,0,0.15)] rounded-sm bg-[rgb(254,254,254)]'>
             <div className='flex items-center ps-3 bg-blue-500 rounded-ss-sm rounded-ee-3xl w-12 h-12 text-white text-lg'>{stepNum}</div>
             <div className='px-4 flex flex-col gap-y-2 items-center relative bottom-3'>
                 <img className='h-16 w-16' src={imgUrl} alt="" />
