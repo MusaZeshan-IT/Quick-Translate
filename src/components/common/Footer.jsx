8bc2cd4bcdf5990895dcfa60011c4d86e0478c90
Musa Zeshan <musazeshan.it@gmail.com>
2024-06-20 21:18:23 +0500

    The footer section has been built.

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,9 +1,53 @@
 import React from 'react';
+import Logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-            <h1 className='text-center text-3xl'>Footer</h1>
+        <div className='bg-[rgb(25,32,87)] h-[420px] mt-10 px-28 pt-10'>
+            <div className='w-full h-full'>
+                <div className='grid grid-cols-2'>
+                    <div>
+                        <div className='flex items-center'>
+                            <img className='h-[44px] me-2 rounded-[50%]' src={Logo} alt="" />
+                            <h2 className='text-white text-[24px] font-semibold tracking-wide'>Quick Translate</h2>
+                        </div>
+                        <p className='text-[rgb(204,204,204)] text-[15px] w-96 mt-6 leading-[25px]'>Now you know that we are the best translating agnecy that you can find. We always aim not to leave any of our customers unsatisfied with our services.</p>
+                        <div className='flex gap-x-5 mt-6'>
+                            <i className='fa-brands fa-facebook-f text-white text-2xl'></i>
+                            <i className='fa-brands fa-twitter text-white text-2xl'></i>
+                            <i className='fa-brands fa-instagram text-white text-2xl'></i>
+                            <i className='fa-brands fa-linkedin-in text-white text-2xl'></i>
+                        </div>
+                    </div>
+                    <div className='flex justify-center mt-2'>
+                        <div className='flex flex-col gap-y-14'>
+                            <div className='flex flex-col'>
+                                <div>
+                                    <h3 className='text-white font-semibold tracking-widest'>NAVIGATE</h3>
+                                    <div className='flex text-[rgb(204,204,204)] mt-4 gap-x-7'>
+                                        <Link>Home</Link>
+                                        <Link>About Us</Link>
+                                        <Link>FAQ</Link>
+                                        <Link>Services</Link>
+                                        <Link>Blog</Link>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='flex flex-col'>
+                                <div>
+                                    <h3 className='text-white font-semibold tracking-widest'>CONTACT US</h3>
+                                    <div className='flex flex-col text-[rgb(204,204,204)] mt-4 gap-y-1'>
+                                        <p className='tracking-wide'>+92 3079410053</p>
+                                        <p>zeshan.qaiser@gmail.com</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <p className='border-t pt-4 text-[rgb(204,204,204)] text-[15px] mt-20'>Copyright &copy; 2024 Quick Translate. All rights reserved.</p>
+            </div>
         </div>
     );
 }
