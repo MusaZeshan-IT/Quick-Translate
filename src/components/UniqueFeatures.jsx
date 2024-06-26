3e54abd7578c1a4c42ba59eda4773ac0fa1d28b7
Musa Zeshan <musazeshan.it@gmail.com>
2024-06-26 12:00:08 +0500

    The image of the unique features at the home page has been fixed accordingly to all screen sizes.

--- src/components/UniqueFeatures.jsx
+++ src/components/UniqueFeatures.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import UniqueImg from '../assets/uniqueFeatures.jpeg'
-import UniqueImgTab from '../assets/uniqueFeatures-tab.jpeg'
+import UniqueImgSm from '../assets/uniqueFeatures/uF-sm.jpeg'
+import UniqueImgTab from '../assets/uniqueFeatures/uF-tab.jpeg'
+import UniqueImgXl from '../assets/uniqueFeatures/uF-xl.jpeg'
 import Feature from './Feature';
 import Feature1 from '../assets/uniqueFeatures/quality1.png'
 import Feature2 from '../assets/uniqueFeatures/quality2.png'
@@ -8,12 +9,13 @@ import Feature3 from '../assets/uniqueFeatures/quality3.png'
 
 const UniqueFeatures = () => {
     return (
-        <div className='flex lg:flex-row-reverse flex-col lg:gap-x-24 gap-y-16'>
-            <div className='xl:w-[57%] lg:w-1/2 w-full border-8 border-blue-500 lg:block flex xl:h-[400px] lg:h-[500px] md:h-[520px] xs-custom:h-[370px] 2xs-custom:h-[500px] h-[380px] mt-10'>
-                <img className='xl:hidden lg:block xs-custom:hidden block lg:h-[550px] h-full w-full lg:relative bottom-10 left-10' src={UniqueImg} alt="" />
-                <img className='xl:block lg:hidden xs-custom:block hidden xl:h-[450px] lg:h-full w-full lg:relative bottom-10 left-10' src={UniqueImgTab} alt="" />
+        <div className='flex lg:flex-row flex-col lg:gap-x-24 gap-y-16'>
+            <div className='xl:w-[57%] lg:w-[52%] w-full border-8 border-blue-500 lg:block flex xl:h-[500px] lg:h-[500px] md:h-[520px] xs-custom:h-[370px] 2xs-custom:h-[500px] h-[380px] mt-10'>
+                <img className='xl:block hidden xl:h-[550px] w-full lg:relative bottom-10 left-10' src={UniqueImgXl} alt="" />
+                <img className='lg:hidden xs-custom:block hidden xl:h-[450px] lg:h-full w-full lg:relative bottom-10 left-10' src={UniqueImgTab} alt="" />
+                <img className='xl:hidden lg:block xs-custom:hidden block lg:h-[550px] h-full w-full lg:relative bottom-10 left-10' src={UniqueImgSm} alt="" />
             </div>
-            <div className='xl:w-[43%] lg:w-1/2'>
+            <div className='xl:w-[43%] lg:w-[48%] w-full'>
                 <h3 className='text-[rgb(60,146,255)] font-semibold text-[16.5px] tracking-wide'>High Professional Translation Agency</h3>
                 <h2 className='text-[rgb(19,40,126)] text-3xl font-black mt-3 xs-custom:xs-custom:w-[500px] leading-10'>Order Professional Translations & Interpreting in Just a Few Clicks</h2>
                 <p className='lg:mt-6 mt-4 text-[rgb(129,135,150)] leading-7'>Our team of professional translators and interpreters are ready to help you with any kind of translation. We have a team of translators who are well-trained and experienced.</p>
