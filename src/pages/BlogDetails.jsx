5816a983ef9f66dbd9a8d77c5bdc031a6a29537c
Musa Zeshan <musazeshan.it@gmail.com>
2024-07-06 17:00:00 +0500

    Handled the case when a specific blog does not exist.

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -4,16 +4,22 @@ import BlogDetailsBgImage from '../assets/heroImages/blog-details-hero-bg.jpg';
 import PostDetailsCard from '../components/blog-details/PostDetailsCard';
 import { useParams } from 'react-router-dom';
 import BlogList from '../helpers/BlogList';
+import Error404 from './Error404';
 
 const BlogDetails = () => {
     const { blogId } = useParams();
     const blog = BlogList.find((blog) => blog.id === Number(blogId));
 
+    // If blog does not exist, render the Error404 component
+    if (!blog) {
+        return <Error404 />;
+    }
+
     return (
         <div className='flex flex-col gap-y-24 pb-28 bg-gray-100'>
             <Navbar isPrevPage={true} previousPageName='blog' pageName='Blog Details' bgImgUrl={BlogDetailsBgImage} />
             <div className='2xl:px-28 xl-custom:px-24 xl:px-20 lg-custom:px-14 lg:px-12 md-custom:px-10 2xs-custom:px-8 px-6'>
-                <PostDetailsCard blog={blog}/>
+                <PostDetailsCard blog={blog} />
             </div>
         </div>
     );
